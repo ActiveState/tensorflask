e6c93ff31dce50b744ba7d11dd7eefbb7ff0c7eb
Pete Garcin <peteg@activestate.com>
2017-11-27 13:52:07 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,133 @@
+# app.py
+#
+# A simple example of hosting a TensorFlow model as a Flask service
+#
+# Copyright 2017 ActiveState Software Inc.
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+import random
+import time
+
+from flask import Flask, jsonify, request
+from scipy.misc import imread, imresize
+
+import argparse
+import sys
+
+import numpy as np
+import tensorflow as tf
+
+app = Flask(__name__)
+
+def load_graph(model_file):
+  graph = tf.Graph()
+  graph_def = tf.GraphDef()
+
+  with open(model_file, "rb") as f:
+    graph_def.ParseFromString(f.read())
+  with graph.as_default():
+    tf.import_graph_def(graph_def)
+
+  return graph
+
+def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
+				input_mean=0, input_std=255):
+  input_name = "file_reader"
+  output_name = "normalized"
+  file_reader = tf.read_file(file_name, input_name)
+  if file_name.endswith(".png"):
+    image_reader = tf.image.decode_png(file_reader, channels = 3,
+                                       name='png_reader')
+  elif file_name.endswith(".gif"):
+    image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+                                                  name='gif_reader'))
+  elif file_name.endswith(".bmp"):
+    image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+  else:
+    image_reader = tf.image.decode_jpeg(file_reader, channels = 3,
+                                        name='jpeg_reader')
+  float_caster = tf.cast(image_reader, tf.float32)
+  dims_expander = tf.expand_dims(float_caster, 0);
+  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+  sess = tf.Session()
+  result = sess.run(normalized)
+
+  return result
+
+def load_labels(label_file):
+  label = []
+  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  for l in proto_as_ascii_lines:
+    label.append(l.rstrip())
+  return label
+
+@app.route('/')
+def classify():
+    file_name = request.args['file']
+
+    t = read_tensor_from_image_file(file_name,
+                                  input_height=input_height,
+                                  input_width=input_width,
+                                  input_mean=input_mean,
+                                  input_std=input_std)
+        
+    with tf.Session(graph=graph) as sess:
+        start = time.time()
+        results = sess.run(output_operation.outputs[0],
+                      {input_operation.outputs[0]: t})
+        end=time.time()
+        results = np.squeeze(results)
+
+        top_k = results.argsort()[-5:][::-1]
+        labels = load_labels(label_file)
+
+    print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
+
+    for i in top_k:
+        print(labels[i], results[i])
+
+    return jsonify(labels,results.tolist())
+
+if __name__ == '__main__':
+    # TensorFlow configuration/initialization
+    model_file = "retrained_graph.pb"
+    label_file = "retrained_labels.txt"
+    input_height = 224
+    input_width = 224
+    input_mean = 128
+    input_std = 128
+    input_layer = "input"
+    output_layer = "final_result"
+
+    # Load TensorFlow Graph from disk
+    graph = load_graph(model_file)
+
+    # Grab the Input/Output operations
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name);
+    output_operation = graph.get_operation_by_name(output_name);
+
+    # Initialize the Flask Service
+    # Obviously, disable Debug in actual Production
+    app.run(debug=True, port=8000)
+
